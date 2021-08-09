42f9a5fcc4284bf56d4e90b1a38ca5d7e9a02da3
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-08-09 11:42:21 -0500

    Add project files.

---
+++ ASP_Dot_Net_MVC_Aug_2021/Controllers/TestController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Dot_Net_MVC_Aug_2021.Controllers
+{
+    public class TestController : Controller
+    {
+        // [Route("foo/bar")]
+        public string Index()
+        {
+            return "Hello Controller";
+        }
+
+        public IActionResult details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound($"There is no id = {id}");
+            }
+
+            return Ok($"You sent id = {id}");
+        }
+
+        public IActionResult status()
+        {
+            // return BadRequest("Bad request");
+            // return Unauthorized("Hello Unauthorized User");
+            // return new UnsupportedMediaTypeResult();
+
+            return Redirect("details/1234");
+        }
+
+        public IActionResult JsonResult()
+        {
+            return Json( new { 
+                message = "This is a JSON message",
+                date = DateTime.Now
+            });
+        }
+
+        public IActionResult ContentResult()
+        {
+            return Content("<h1>This is a content</h1>");
+        }
+
+        public IActionResult MyAction()
+        {
+            List<string> list = new List<string> { 
+                "string #1",
+                "string #2",
+                "string #3",
+                "string #4",
+                "string #5",
+            };
+
+            return View(list);
+        }
+    }
+}
---
+++ ASP_Dot_Net_MVC_Aug_2021/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_Dot_Net_MVC_Aug_2021.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
